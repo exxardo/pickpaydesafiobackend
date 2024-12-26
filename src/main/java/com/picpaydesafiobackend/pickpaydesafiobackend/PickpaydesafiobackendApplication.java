4d06696a5421183142b7272ef39c05ed1e332a18
exxardo <eduardoramirodematos@gmail.com>
2024-12-26 00:15:58 -0300

    Criação do Projeto

---
+++ src/main/java/com/picpaydesafiobackend/pickpaydesafiobackend/PickpaydesafiobackendApplication.java
@@ -0,0 +1,13 @@
+package com.picpaydesafiobackend.pickpaydesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpaydesafiobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpaydesafiobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaydesafiobackend/pickpaydesafiobackend/PickpaydesafiobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaydesafiobackend.pickpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpaydesafiobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
